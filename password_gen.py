4e551ede247574d2ba914c3efcba3cf5762e5e04
Aleks B <aleksandir@me.com>
2023-12-29 23:33:46 +1030

    Add password generation and history functionality

--- password_gen.py
+++ password_gen.py
@@ -1,13 +1,14 @@
-from password_maker import password_maker
 from typer import Option, Typer, echo
+
+from src.password_maker import password_maker
 
 app = Typer()
 
 # https://pravash-techie.medium.com/python-better-clis-with-typer-a8783fafec6c#:~:text=What%20is%20Typer,arguments%20from%20the%20underlying%20functions.
 
 
-@app.command()
-def password(
+@app.command(name="gen")
+def generate(
     length: int = Option(
         10, "--length", "--len", help="The length of the password", show_default=True
     ),
@@ -25,13 +26,37 @@ def password(
         no_special_characters (bool): Exclude special characters if True. Default is False.
         no_numbers (bool): Exclude numbers if True. Default is False.
     """
-    for _ in range(count):
-        echo(
+    passwords = []
+    for i in range(count):
+        passwords.append(
             password_maker(
-                length=length, special_characters=not no_special_characters, numbers=not no_numbers
+                length=length,
+                special_characters=not no_special_characters,
+                numbers=not no_numbers,
             )
         )
+        echo(f"Password {i+1}: {passwords[i]}")
+
+    with open("generated_passwords.txt", "a") as f:
+        for password in passwords:
+            f.write(password + "\n")
+
+
+@app.command()
+def history():
+    """
+    Show the history of generated passwords.
+    """
+    if past_passwords:
+        for password in past_passwords:
+            echo(password)
+    else:
+        echo("No passwords generated yet.")
 
 
 if __name__ == "__main__":
+    past_passwords = []
+    with open("generated_passwords.txt", "r") as f:
+        past_passwords = f.read().splitlines()
+
     app()
