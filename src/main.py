8be321cfd864e16f0023e9299610a9ca509b1c78
Aleks B <aleksandir@me.com>
2023-12-29 22:54:53 +1030

    Refactor password command in main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
-import typer
 from password_maker import password_maker
+from typer import Option, Typer, echo
 
-app = typer.Typer()
+app = Typer()
 
 # https://pravash-techie.medium.com/python-better-clis-with-typer-a8783fafec6c#:~:text=What%20is%20Typer,arguments%20from%20the%20underlying%20functions.
 
 
 @app.command()
 def password(
-    length: int = typer.Option(
+    length: int = Option(
         10, "--length", "--len", help="The length of the password", show_default=True
     ),
-    no_special_characters: bool = typer.Option(
+    no_special_characters: bool = Option(
         False, "--no-special-characters", "-nsc", help="Exclude special characters"
     ),
-    no_numbers: bool = typer.Option(False, "--no-numbers", "-nn", help="Exclude numbers"),
-    count: int = typer.Option(1, "--count", "-c", help="Number of passwords to generate"),
+    no_numbers: bool = Option(False, "--no-numbers", "-nn", help="Exclude numbers"),
+    count: int = Option(1, "--count", "-c", help="Number of passwords to generate"),
 ):
     """
     Generate a random password.
@@ -26,7 +26,7 @@ def password(
         no_numbers (bool): Exclude numbers if True. Default is False.
     """
     for _ in range(count):
-        typer.echo(
+        echo(
             password_maker(
                 length=length, special_characters=not no_special_characters, numbers=not no_numbers
             )
