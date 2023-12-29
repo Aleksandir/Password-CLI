108acd06e10ffa4d498afa188e7e7386bcc7966c
Aleks B <aleksandir@me.com>
2023-12-29 22:44:52 +1030

    Add password generation functionality to main.py

--- src/main.py
+++ src/main.py
@@ -1,35 +1,32 @@
 import typer
+from password_maker import password_maker
 
 app = typer.Typer()
 
-
-@app.command()
-def hello_world():
-    """
-    Prints 'hello world' to the console.
-    """
-    print("hello world")
-
-
 # https://pravash-techie.medium.com/python-better-clis-with-typer-a8783fafec6c#:~:text=What%20is%20Typer,arguments%20from%20the%20underlying%20functions.
-@app.command()
-def hello(name):
-    print(f"hello {name}")
 
 
 @app.command()
-def greet2(name: str = typer.Argument("world", help="The name to greet", show_default=True)):
-    typer.echo(f"Hello, {name}!")
+def password(
+    length: int = typer.Option(10, help="The length of the password", show_default=True),
+    no_special_characters: bool = typer.Option(
+        False, "--no-special-characters", "-nsc", help="Exclude special characters"
+    ),
+    no_numbers: bool = typer.Option(False, "--no-numbers", "-nn", help="Exclude numbers"),
+):
+    """
+    Generate a random password.
 
-
-@app.command()
-def backup(database: str, output_dir: str, force: bool = False):
-    if force:
-        typer.echo("Forced backup requested!")
-    else:
-        typer.echo("Regular backup requested.")
-    typer.echo(f"Database: {database}")
-    typer.echo(f"Output directory: {output_dir}")
+    Args:
+        length (int): The length of the password. Default is 10.
+        no_special_characters (bool): Exclude special characters if True. Default is False.
+        no_numbers (bool): Exclude numbers if True. Default is False.
+    """
+    typer.echo(
+        password_maker(
+            length=length, special_characters=not no_special_characters, numbers=not no_numbers
+        )
+    )
 
 
 if __name__ == "__main__":
